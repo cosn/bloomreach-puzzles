762159a6539730487fae36c49c44596913819479
Cosmin Nicolaescu <cosmin.nicolaescu@gmail.com>
2013-09-14 17:23:46 -0700

    clean up code

--- WineTasting/winetasting.rb
+++ WineTasting/winetasting.rb
@@ -1,57 +1,64 @@
 require "csv"
 
-def wines(file)
-	wines = Hash.new { |h, k| h[k] = [] }
-	people = Hash.new { |h, k| h[k] = [] }
-	result = Hash.new { |h, k| h[k] = [] }
-	total = 0
-	incomplete = 0
+@winesPerPerson = 3
 
-	CSV.foreach(file, { :col_sep => "\t" }) do |row|
-		wines[row[1]] << row[0]
-		people[row[0]] << row[1]
-	end
+def wines(input, output)
+    wines = Hash.new { |h, k| h[k] = [] }
+    people = Hash.new { |h, k| h[k] = [] }
+    result = Hash.new { |h, k| h[k] = [] }
+    total = 0
+    incomplete = 0
 
-	people.each do |p, w|
-		w.each do |wine|
-			break if result[p].count == 3
-			next if wines[wine] == nil || wines[wine].count > 1
+    # load the file in two dimensions: 
+    #   wines with all the people that want them
+    #   and people with all the wines they wany
+    CSV.foreach(input, { :col_sep => "\t" }) do |row|
+        wines[row[1]] << row[0]
+        people[row[0]] << row[1]
+    end
 
-			result[p] << wine
-			wines[wine] = nil
-			total += 1
-		end
-	end
+    # go through each person and have them buy wines
+    # if they are the only one who want a wine, give it to them
+    # do not give anyone wines under contention by several people
+    people.each do |p, w|
+        w.each do |wine|
+            break if result[p].count == @winesPerPerson
+            next if wines[wine] == nil || wines[wine].count > 1
 
-	wines.each do |w, p|
-		next if p == nil
+            # count the total wines sold and mark them as such
+            result[p] << wine
+            wines[wine] = nil
+            total += 1
+        end
+    end
 
-		p.each do |person|
-			next if result[person].count == 3
+    # whatever wines are left are under contention
+    # give each wine to the first person in the list that wants it
+    # and still hasn't received a wine
+    wines.each do |w, p|
+        next if p == nil
 
-			result[person] << w
-			p = nil
-			total += 1
-			break
-		end
-	end
+        p.each do |person|
+            next if result[person].count == @winesPerPerson
 
-	#wines.each { |k, v| puts "#{k} is wanted by #{v}" if v.count > 1 }
-	#people.each { |k, v| puts "#{k} wants #{v}" }
-	#result.each { |k, v| puts "#{k} got #{v}" }
-	#result.each { |k, v| puts "#{k} did not get his wines (#{v})" if v.count != 3 }
-	#result.each_value { |v| incomplete += 1 if v.count != 3 }
-	output = File.open("out.txt", 'w')
-	output.write("#{total}\n")
-	result.each { |k, v| v.each { |vv| output.write("#{k}\t#{vv}\n") } }
+            result[person] << w
+            p = nil
+            total += 1
+            break
+        end
+    end
 
-	percentSold = total.to_f / wines.length.to_f * 100.0
-	percentIncomplete = incomplete / people.length.to_f * 100.0
-	puts "Sold #{total} out of #{wines.length} wines (#{percentSold.round(2)}%)"
-	puts "#{incomplete} people did not get all their wines (#{percentIncomplete}%)"
+    out = File.open(output, 'w')
+    out.write("#{total}\n")
+    result.each { |k, v| v.each { |vv| out.write("#{k}\t#{vv}\n") } }
+    puts "Results saved in #{output}"
 
+    percentSold = total.to_f / wines.length.to_f * 100.0
+    percentIncomplete = incomplete / people.length.to_f * 100.0
+    puts "Sold #{total} out of #{wines.length} wines (#{percentSold.round(2)}%)"
+    puts "#{incomplete} people did not get all #{@winesPerPerson} wines (#{percentIncomplete}%)"
 end
 
-wines("person_wine_3.txt")
-#wines("person_wine_4.txt")
-#wines("person_wine_5.txt")
+wines("person_wine_3.txt", "out.txt")
+#wines("person_wine_4.txt", "out.txt")
+#wines("person_wine_5.txt", "out.txt")
